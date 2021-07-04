ff6167eb3b7e54194fb2856838bf7f50ee77d94c
sgykim15 <sgykim36@gmail.com>
2021-07-04 16:36:48 +0900

    Main Game instance commit

---
+++ Source/ActionsForMe/AFMGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AFMGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONSFORME_API UAFMGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
